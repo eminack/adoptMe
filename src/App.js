db7b2823d5d4bc33df2d8410ad555b9cf1fcf1c0
eminack <kathal.sameer@gmail.com>
2020-09-24 14:49:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+const Pet = ({name,animal,breed}) => {
+return React.createElement("div",{},[
+    React.createElement("h1",{},name),
+    React.createElement("h2",{},animal),
+    React.createElement("h2",{},breed)    
+    ]);
+};
+
+
+const App = () => {
+    return React.createElement(
+        "div",  
+        {id :"something element"},  
+        [
+            React.createElement("h1",{},"Adopt Me!"),
+            React.createElement(Pet, { 
+                name: "Luna", 
+                animal:"Dog", 
+                breed:"german shephard"
+            }),
+            React.createElement(Pet, {   
+                name:"buddy",
+                animal:"Dog",  
+                breed:"Labrador"
+            }),
+            React.createElement(Pet, { 
+                  name:"don",
+                  animal:"Dog", 
+                  breed:"mixed"
+            })
+        ]
+    );
+};
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("root")
+);
