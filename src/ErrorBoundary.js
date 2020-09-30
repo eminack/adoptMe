86c1016517aedceaa72ef42ddf84cc9c9590b3ee
eminack <kathal.sameer@gmail.com>
2020-09-30 17:22:20 +0530

    Add ErrorBoundary to details route

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "@reach/router";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false, redirect: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("ErroBoundary caught an error", error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with the listing <Link to="/">Click here</Link> to
+          go back to homepage or wait 5 seconds.
+        </h1>
+      );
+    }
+    return this.props.children; //this.props.children will be all the children components inside props
+  }
+}
+
+export default ErrorBoundary;
