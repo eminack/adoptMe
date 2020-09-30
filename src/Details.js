97ed1037a0dda7cec3723eccb016aed2dd151c5b
eminack <kathal.sameer@gmail.com>
2020-09-30 15:06:04 +0530

    Add reach Router

---
+++ src/Details.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Details = (props) => {
+  return (
+    <pre>
+      <code>{JSON.stringify(props, null, 4)}</code>
+    </pre>
+  );
+  // return <h1>hi</h1>;
+};
+
+export default Details;
