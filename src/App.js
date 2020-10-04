57e70a2c2503e0173f7830d6af8c4436fdf09f5e
eminack <kathal.sameer@gmail.com>
2020-10-04 17:15:49 +0530

    code-splitting implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Link, Router } from "@reach/router";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+//import SearchParams from "./SearchParams";
+
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -14,10 +17,12 @@ const App = () => {
           <header>
             <Link to="/">Adopt Me! </Link>
           </header>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense fallback={<h1>Loading Route...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -36,7 +36,6 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <h1>{location}</h1>
       <form
         onSubmit={(e) => {
           e.preventDefault();
